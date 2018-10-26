a66b3e19150ecc4bfd06d3207d21cc32ab2eecd7
salaboy <salaboy@gmail.com>
2018-10-26 15:41:22 +0100

    updating more details

--- src/main/java/org/activiti/cloud/runtime/bundle/RuntimeBundleApplication.java
+++ src/main/java/org/activiti/cloud/runtime/bundle/RuntimeBundleApplication.java
@@ -11,5 +11,6 @@ public class RuntimeBundleApplication {
     public static void main(String[] args) {
         SpringApplication.run(RuntimeBundleApplication.class,
                               args);
+
     }
 }
